f7b044158d7f7cabea1cb3ec36127b708ebd9190
ISTEK <tamdacoder@gmail.com>
2025-06-22 15:41:55 -0500

    Create import.header.js

---
+++ assets/scripts/import.header.js
@@ -0,0 +1,15 @@
+const header = `
+<header>
+  <div class="left">
+    <img class="headerlogo" src="/assets/images/tesseract-lockup-1.transparent.png" alt="TESSERACT">
+    <a class="hl" href="https://docs.tesseractcoin.org/">Docs</a>
+    <a class="hl" href="/products/mining">Mining</a>
+    <a class="hl" href="/products/wallet">Wallet</a>
+    <a class="hl" href="https://status.tesseractcoin.org/">Status</a>
+    <a class="hl" href="/changelog.html">Changelog</a>
+  </div>
+  <div class="right">
+    <!-- Later -->
+  </div>
+</header>
+`;
